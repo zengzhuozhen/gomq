c8bd7ae28122ba38cde6d71f3a70880636206a30
zengzhuozhen <514551226@qq.com>
2021-04-13 20:04:13 +0800

    fix:concureeny map write

--- Makefile
+++ Makefile
@@ -10,9 +10,9 @@ build:
 	go build -o gomqctl cmd/main.go
 
 benchmarkTest:
-	ab -n 1000 -c 10 -T 'application/x-www-form-urlencode' -p benchmark/input-level-1.txt  http://localhost:8000/pub > benchmark/output-level-1.txt
-	ab -n 1000 -c 10 -T 'application/x-www-form-urlencode' -p benchmark/input-level-2.txt http://localhost:8000/pub > benchmark/output-level-2.txt
-	ab -n 1000 -c 10 -T 'application/x-www-form-urlencode' -p benchmark/input-level-3.txt  http://localhost:8000/pub > benchmark/output-level-3.txt
+	ab -n 1000 -c 10 -T 'application/json' -p benchmark/input-level-1.txt  http://localhost:8000/pub > benchmark/output-level-1.txt
+	ab -n 1000 -c 10 -T 'application/json' -p benchmark/input-level-2.txt  http://localhost:8000/pub > benchmark/output-level-2.txt
+	ab -n 1000 -c 10 -T 'application/json' -p benchmark/input-level-3.txt  http://localhost:8000/pub > benchmark/output-level-3.txt
 
 
 
--- common/queue.go
+++ common/queue.go
@@ -6,26 +6,27 @@ import (
 )
 
 type Queue struct {
-	Local          map[string][]MessageUnit // Local[Topic][Message_1,Message_2]
-	mu             *sync.RWMutex
+	Local map[string][]MessageUnit // Local[Topic][Message_1,Message_2]
+	mu    *sync.RWMutex
 }
 
 func NewQueue() *Queue {
 	return &Queue{
-		Local:          make(map[string][]MessageUnit),
-		mu:             new(sync.RWMutex),
+		Local: make(map[string][]MessageUnit),
+		mu:    new(sync.RWMutex),
 	}
 }
 
 func (q *Queue) Push(messageUnit MessageUnit) {
-	q.mu.RLock()
+	q.mu.Lock()
+	// todo 并发写map问题
 	q.Local[messageUnit.Topic] = append(q.Local[messageUnit.Topic], messageUnit)
-	q.mu.RUnlock()
+	q.mu.Unlock()
 }
 
 func (q *Queue) Pop(topic string, position int64) (message MessageUnit, err error) {
-	q.mu.RLock()
-	defer q.mu.RUnlock()
+	q.mu.Lock()
+	defer q.mu.Unlock()
 	if len(q.Local[topic]) == 0 {
 		return MessageUnit{}, errors.New("目前还没有消息")
 	} else if int64(len(q.Local[topic])) <= position {
